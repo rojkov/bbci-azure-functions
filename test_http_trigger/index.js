7b555862f4caa15314dabf521ac66f91b4368ccf
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2017-07-18 13:46:20 +0300

    clarification for testing purposes

--- test_http_trigger/index.js
+++ test_http_trigger/index.js
@@ -10,7 +10,7 @@ module.exports = function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body."
+            body: "Please pass a name on the query string or in the request body. (?name=name)"
         };
     }
     context.done();
