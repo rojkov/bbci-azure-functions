2605f8b0da9485f29268e5595b328a2cbd888fc2
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2017-07-28 10:56:24 +0300

    fix tests

--- github_trigger/build.sh
+++ github_trigger/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+mv node_modules node_modules.bak
 browserify --node deps.js > deps.bundle.js
 uglifyjs -o deps.bundle.min.js deps.bundle.js
+rm -r node_modules
+mv node_modules.bak node_modules
