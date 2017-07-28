98c606e21099b926ea104d33b25f0faf1dd44ba2
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2017-07-28 15:43:40 +0300

    unuglify

--- github_trigger/build.sh
+++ github_trigger/build.sh
@@ -2,7 +2,7 @@
 
 mv node_modules node_modules.bak
 npm install
-browserify --node deps.js > deps.bundle.js
-uglifyjs -o deps.bundle.min.js deps.bundle.js
+browserify --node deps.js > deps.bundle.min.js
+#uglifyjs -o deps.bundle.min.js deps.bundle.js
 rm -r node_modules
 mv node_modules.bak node_modules
